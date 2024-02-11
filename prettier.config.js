c3788b95247f0e833c75a67823eaac0f2b37a8ce
gevorah <jhonsebastianijajiortiz@gmail.com>
2024-02-10 22:22:14 -0500

    build: add astro

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: 'avoid',
+  endOfLine: 'crlf',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+};
